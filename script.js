798a0dcc3bdeaee3626c6a6b2b6a577dee210c8d
alifsyfulislam <alifsyfulislam@gmail.com>
2019-04-01 16:31:31 +0600

    added

---
+++ script.js
@@ -0,0 +1,395 @@
+let balloonImg = document.querySelector('.balloon_img');
+let ball2 = document.querySelector('.ball_2');
+let ball5 = document.querySelector('.ball_5');
+let ball7 = document.querySelector('.ball_7');
+let ball9 = document.querySelector('.ball_9');
+let ballA = document.querySelector('.ball_a');
+
+function gPlay() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg.style.display = "none";
+
+    ball2.className = ball2.className.replace('hidden', 'slideTop');
+    ball5.className = ball5.className.replace('hidden', 'slideRight');
+    ball7.className = ball7.className.replace('hidden', 'slideLeft');
+    ball9.className = ball9.className.replace('hidden', 'slideBottom');
+    ballA.className = ballA.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        ball2.style.display = "none";
+        ball5.style.display = "none";
+        ball7.style.display = "none";
+        ball9.style.display = "none";
+        ballA.style.display = "none";
+    },500)
+}
+
+
+let balloonImg2 = document.querySelector('.balloon_img2');
+let ball1 = document.querySelector('.ball_1');
+let ball3 = document.querySelector('.ball_3');
+let ball4 = document.querySelector('.ball_4');
+let ball6 = document.querySelector('.ball_6');
+let ballB = document.querySelector('.ball_b');
+
+function gPlay2() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg2.style.display = "none";
+
+    ball1.className = ball1.className.replace('hidden', 'slideTop');
+    ball3.className = ball3.className.replace('hidden', 'slideRight');
+    ball4.className = ball4.className.replace('hidden', 'slideLeft');
+    ball6.className = ball6.className.replace('hidden', 'slideBottom');
+    ballB.className = ballB.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        ball1.style.display = "none";
+        ball3.style.display = "none";
+        ball4.style.display = "none";
+        ball6.style.display = "none";
+        ballB.style.display = "none";
+    },500)
+}
+
+let balloonImg3 = document.querySelector('.balloon_img3');
+let ball8 = document.querySelector('.ball_8');
+let ball10 = document.querySelector('.ball_10');
+let ball11 = document.querySelector('.ball_11');
+let ball12 = document.querySelector('.ball_12');
+let ballC = document.querySelector('.ball_c');
+
+
+function gPlay3() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg3.style.display = "none";
+
+    ball8.className = ball8.className.replace('hidden', 'slideTop');
+    ball10.className = ball10.className.replace('hidden', 'slideRight');
+    ball11.className = ball11.className.replace('hidden', 'slideLeft');
+    ball12.className = ball12.className.replace('hidden', 'slideBottom');
+    ballC.className = ballC.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        ball8.style.display = "none";
+        ball10.style.display = "none";
+        ball11.style.display = "none";
+        ball12.style.display = "none";
+        ballC.style.display = "none";
+    },500)
+}
+
+
+let balloonImg4 = document.querySelector('.balloon_img4');
+let balld = document.querySelector('.ball_d');
+let balle = document.querySelector('.ball_e');
+let ballf = document.querySelector('.ball_f');
+let ballg = document.querySelector('.ball_g');
+let ballh = document.querySelector('.ball_h');
+
+function gPlay4() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg4.style.display = "none";
+
+    balld.className = balld.className.replace('hidden', 'slideTop');
+    balle.className = balle.className.replace('hidden', 'slideRight');
+    ballf.className = ballf.className.replace('hidden', 'slideLeft');
+    ballg.className = ballg.className.replace('hidden', 'slideBottom');
+    ballh.className = ballh.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        balld.style.display = "none";
+        balle.style.display = "none";
+        ballf.style.display = "none";
+        ballg.style.display = "none";
+        ballh.style.display = "none";
+    },500)
+}
+
+
+let balloonImg5 = document.querySelector('.balloon_img5');
+let balli = document.querySelector('.ball_i');
+let ballj = document.querySelector('.ball_j');
+let ballk = document.querySelector('.ball_k');
+let balll = document.querySelector('.ball_l');
+let ballm = document.querySelector('.ball_m');
+
+function gPlay5() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg5.style.display = "none";
+
+    balli.className = balli.className.replace('hidden', 'slideTop');
+    ballj.className = ballj.className.replace('hidden', 'slideRight');
+    ballk.className = ballk.className.replace('hidden', 'slideLeft');
+    balll.className = balll.className.replace('hidden', 'slideBottom');
+    ballm.className = ballm.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        balli.style.display = "none";
+        ballj.style.display = "none";
+        ballk.style.display = "none";
+        balll.style.display = "none";
+        ballm.style.display = "none";
+    },500)
+}
+
+
+let balloonImg6 = document.querySelector('.balloon_img6');
+let balln = document.querySelector('.ball_n');
+let ballo = document.querySelector('.ball_o');
+let ballp = document.querySelector('.ball_p');
+let ballq = document.querySelector('.ball_q');
+let ballr = document.querySelector('.ball_r');
+
+function gPlay6() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg6.style.display = "none";
+
+    balln.className = balln.className.replace('hidden', 'slideTop');
+    ballo.className = ballo.className.replace('hidden', 'slideRight');
+    ballp.className = ballp.className.replace('hidden', 'slideLeft');
+    ballq.className = ballq.className.replace('hidden', 'slideBottom');
+    ballr.className = ballr.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        balln.style.display = "none";
+        ballo.style.display = "none";
+        ballp.style.display = "none";
+        ballq.style.display = "none";
+        ballr.style.display = "none";
+    },500)
+}
+
+
+let balloonImg7 = document.querySelector('.balloon_img7');
+let balls = document.querySelector('.ball_s');
+let ballt = document.querySelector('.ball_t');
+let ballu = document.querySelector('.ball_u');
+let ballv = document.querySelector('.ball_v');
+let ballw = document.querySelector('.ball_w');
+
+function gPlay7() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg7.style.display = "none";
+
+    balls.className = balls.className.replace('hidden', 'slideTop');
+    ballt.className = ballt.className.replace('hidden', 'slideRight');
+    ballu.className = ballu.className.replace('hidden', 'slideLeft');
+    ballv.className = ballv.className.replace('hidden', 'slideBottom');
+    ballw.className = ballw.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        balls.style.display = "none";
+        ballt.style.display = "none";
+        ballu.style.display = "none";
+        ballv.style.display = "none";
+        ballw.style.display = "none";
+    },500)
+}
+
+
+let balloonImg8 = document.querySelector('.balloon_img8');
+let ballx = document.querySelector('.ball_x');
+let bally = document.querySelector('.ball_y');
+let ballz = document.querySelector('.ball_z');
+let ballaa = document.querySelector('.ball_aa');
+let ballab = document.querySelector('.ball_ab');
+
+function gPlay8() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg8.style.display = "none";
+
+    ballx.className = ballx.className.replace('hidden', 'slideTop');
+    bally.className = bally.className.replace('hidden', 'slideRight');
+    ballz.className = ballz.className.replace('hidden', 'slideLeft');
+    ballaa.className = ballaa.className.replace('hidden', 'slideBottom');
+    ballab.className = ballab.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        ballx.style.display = "none";
+        bally.style.display = "none";
+        ballz.style.display = "none";
+        ballaa.style.display = "none";
+        ballab.style.display = "none";
+    },500)
+}
+
+
+
+let balloonImg9 = document.querySelector('.balloon_img9');
+let ballac = document.querySelector('.ball_ac');
+let ballad = document.querySelector('.ball_ad');
+let ballae = document.querySelector('.ball_ae');
+let ballaf = document.querySelector('.ball_af');
+let ballag = document.querySelector('.ball_ag');
+
+function gPlay9() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg9.style.display = "none";
+
+    ballac.className = ballac.className.replace('hidden', 'slideTop');
+    ballad.className = ballad.className.replace('hidden', 'slideRight');
+    ballae.className = ballae.className.replace('hidden', 'slideLeft');
+    ballaf.className = ballaf.className.replace('hidden', 'slideBottom');
+    ballag.className = ballag.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        ballac.style.display = "none";
+        ballad.style.display = "none";
+        ballae.style.display = "none";
+        ballaf.style.display = "none";
+        ballag.style.display = "none";
+    },500)
+}
+
+let balloonImg10 = document.querySelector('.balloon_img10');
+let ballah = document.querySelector('.ball_ah');
+let ballai = document.querySelector('.ball_ai');
+let ballaj = document.querySelector('.ball_aj');
+let ballak = document.querySelector('.ball_ak');
+let ballal = document.querySelector('.ball_al');
+
+
+function gPlay10() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg10.style.display = "none";
+
+    ballah.className = ballah.className.replace('hidden', 'slideTop');
+    ballai.className = ballai.className.replace('hidden', 'slideRight');
+    ballaj.className = ballaj.className.replace('hidden', 'slideLeft');
+    ballak.className = ballak.className.replace('hidden', 'slideBottom');
+    ballal.className = ballal.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        ballah.style.display = "none";
+        ballai.style.display = "none";
+        ballaj.style.display = "none";
+        ballak.style.display = "none";
+        ballal.style.display = "none";
+    },500)
+}
+
+
+let balloonImg11 = document.querySelector('.balloon_img11');
+let ballam = document.querySelector('.ball_am');
+let ballan = document.querySelector('.ball_an');
+let ballao = document.querySelector('.ball_ao');
+let ballap = document.querySelector('.ball_ap');
+let ballaq = document.querySelector('.ball_aq');
+
+
+function gPlay11() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg11.style.display = "none";
+
+    ballam.className = ballam.className.replace('hidden', 'slideTop');
+    ballan.className = ballan.className.replace('hidden', 'slideRight');
+    ballao.className = ballao.className.replace('hidden', 'slideLeft');
+    ballap.className = ballap.className.replace('hidden', 'slideBottom');
+    ballaq.className = ballaq.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        ballam.style.display = "none";
+        ballan.style.display = "none";
+        ballao.style.display = "none";
+        ballap.style.display = "none";
+        ballaq.style.display = "none";
+    },500)
+}
+
+
+let balloonImg12 = document.querySelector('.balloon_img12');
+let ballar = document.querySelector('.ball_ar');
+let ballas = document.querySelector('.ball_as');
+let ballat = document.querySelector('.ball_at');
+let ballau = document.querySelector('.ball_au');
+let ballav = document.querySelector('.ball_av');
+
+
+function gPlay12() {
+    score++;
+    scorePoint.innerText = score;
+    balloonImg12.style.display = "none";
+
+    ballar.className = ballar.className.replace('hidden', 'slideTop');
+    ballas.className = ballas.className.replace('hidden', 'slideRight');
+    ballat.className = ballat.className.replace('hidden', 'slideLeft');
+    ballau.className = ballau.className.replace('hidden', 'slideBottom');
+    ballav.className = ballav.className.replace('hidden', 'slideBottom');
+
+    setTimeout(function () {
+        ballar.style.display = "none";
+        ballas.style.display = "none";
+        ballat.style.display = "none";
+        ballau.style.display = "none";
+        ballav.style.display = "none";
+    },500)
+}
+
+
+balloonImg.addEventListener('click', gPlay);
+balloonImg2.addEventListener('click', gPlay2);
+balloonImg3.addEventListener('click', gPlay3);
+balloonImg4.addEventListener('click', gPlay4);
+balloonImg5.addEventListener('click', gPlay5);
+balloonImg6.addEventListener('click', gPlay6);
+balloonImg7.addEventListener('click', gPlay7);
+balloonImg8.addEventListener('click', gPlay8);
+balloonImg9.addEventListener('click', gPlay9);
+balloonImg10.addEventListener('click', gPlay10);
+balloonImg11.addEventListener('click', gPlay11);
+balloonImg12.addEventListener('click', gPlay12);
+
+
+
+
+
+
+let scorePoint = document.querySelector('.score_point');
+var score = 0;
+
+
+var adBox = document.querySelector('.ad_box');
+var elementLive = document.querySelector('.element_live');
+var overLay = document.querySelector('.overlay');
+let counter = 10;
+overLay.addEventListener('dblclick', checkIT);
+
+function checkIT() {
+    overLay.style.display = "none";
+
+    const interval = setInterval(function () {
+        counter--;
+        if (score> 10 && counter < 0){
+            adBox.innerHTML = "You Won!!!!!";
+        }
+        else if(score < 10 && counter < 0){
+            adBox.innerHTML = "You Lose!!!!!";
+        }
+        else{
+            elementLive.innerText=counter;
+        }
+        // if (counter <= -1) {
+        //     if (score >= 10){
+        //         clearInterval(interval);
+        //         adBox.innerHTML = "You Won!";
+        //     }
+        // }
+        // else if(counter == 0){
+        //     adBox.innerHTML = "Game Over!!!!!";
+        // }
+        // else {
+        //     elementLive.innerText=counter;
+        // }
+    },1000)
+}
